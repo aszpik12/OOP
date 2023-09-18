49777f88b9ace677809685dba3b077626d9c86ec
Asztrik <105195963+aszpik12@users.noreply.github.com>
2023-09-18 11:36:32 +0200

    OOP

---
+++ kartyajatek/src/main/java/com/mycompany/kartyajatek/Kartyajatek.java
@@ -0,0 +1,7 @@
+package com.mycompany.kartyajatek;
+
+public class Kartyajatek {
+    public static void main(String[] args) {
+      
+    }
+}
---
+++ kartyajatek/src/main/java/com/mycompany/kartyajatek/Lap.java
@@ -0,0 +1,17 @@
+package com.mycompany.kartyajatek;
+
+public class Lap {
+    
+    private String leiras;
+
+    public Lap(){
+        
+    }
+
+    public String getLeiras() {
+        return leiras;
+    }
+
+
+
+}
